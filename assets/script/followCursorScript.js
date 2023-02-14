5df7b039eecf3c46b6828ef8ec4cf97626dd0608
Vladik Leshko <leshkovladik@gmail.com>
2023-02-14 12:46:14 +0300

    started mouse move

--- assets/script/followCursorScript.js
+++ assets/script/followCursorScript.js
@@ -2,6 +2,8 @@ var item = document.querySelector(".item");
 var headerHeight = document.querySelector("#block").scrollHeight;
 
 block.onmousemove = function (e){
+    var startPoint = document.querySelector(".lazerImage");
+    console.log(startPoint.getBoundingClientRect().x);
     item.style.position = 'fixed';
     item.style.left = e.clientX + -150 + 'px';
     item.style.top = e.clientY + -150 + 'px';
